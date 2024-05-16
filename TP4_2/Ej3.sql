5cddbe32a06f1ee7a34f02a6f865ea18d75aa4c6
nachoperna <105826237+nachoperna@users.noreply.github.com>
2024-05-16 19:22:10 -0300

    Update Ej3.sql

--- TP4_2/Ej3.sql
+++ TP4_2/Ej3.sql
@@ -8,5 +8,31 @@
       (SELECT nro_voluntario FROM historico))) = 1;
 
 -- 2. Liste el id, nombre y apellido de los voluntarios de instituciones asentadas en el continente ‘Europeo’ o que son coordinadores, y que además cumplen con el rol de director de alguna institución. Ordene el resultado alfabéticamente. 
+      SELECT nro_voluntario, nombre, apellido
+      FROM voluntario
+      WHERE nro_voluntario IN
+      (SELECT id_director FROM institucion) AND (id_coordinador IS NULL OR id_institucion IN
+      (SELECT id_institucion FROM institucion WHERE id_direccion IN
+      (SELECT id_direccion FROM direccion WHERE id_pais IN
+      (SELECT id_pais FROM pais WHERE id_continente IN
+      (SELECT id_continente FROM continente WHERE nombre_continente = 'Europeo')))))
+      ORDER BY apellido;
+
+-- 3. Liste todos los voluntarios que no pertenecen a instituciones ubicadas en el continente Europeo.
+      SELECT nro_voluntario, nombre, apellido
+      FROM voluntario
+      WHERE id_institucion IN
+      (SELECT id_institucion FROM institucion WHERE id_direccion IN
+      (SELECT id_direccion FROM direccion WHERE id_pais IN
+      (SELECT id_pais FROM pais WHERE id_continente IN
+      (SELECT id_continente FROM continente WHERE nombre_continente != 'Europeo'))))
+      ORDER BY apellido;
       
+-- 4. Indique los voluntarios que históricamente hayan trabajado para todas las instituciones, ordenando el resultado por nombre de voluntario.
+      SELECT nro_voluntario, id_institucion FROM historico INTERSECT
+      SELECT nro_voluntario, id_institucion FROM voluntario
+      ORDER BY nro_voluntario;
+      -- creo que mal hecho
+
+-- 5. Determine cuáles tareas se están ejecutando en todas las instituciones.
       

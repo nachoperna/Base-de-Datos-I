0ead54e0feb5a01133dc7cdfe43a1019db6221ce
nachoperna <105826237+nachoperna@users.noreply.github.com>
2024-05-31 00:58:12 -0300

    Create Ej-Peliculas.sql

---
+++ TP4_2/Ejercicios Adicionales/Ej-Peliculas.sql
@@ -0,0 +1,31 @@
+-- 7. Liste los identificadores y títulos de aquellas películas que registran la mayor cantidad de entregas y la menor cantidad de ellas, junto con la cantidad respectiva. 
+((SELECT p.codigo_pelicula, p.titulo, e.cantidad
+  FROM pelicula p JOIN renglon_entrega e USING(codigo_pelicula)
+  ORDER BY e.cantidad DESC
+  LIMIT 1)
+  UNION 
+  (SELECT p.codigo_pelicula, p.titulo, e.cantidad
+  FROM pelicula p JOIN renglon_entrega e USING(codigo_pelicula)
+  ORDER BY e.cantidad
+  LIMIT 1));
+  -- sacala del medio
+
+-- 8. Obtenga los datos de los videos que han recibido entregas por parte de distribuidores nacionales y también internacionales, ordenados por razón social 
+SELECT v.*  
+FROM video v JOIN entrega e USING(id_video) JOIN distribuidor d USING(id_distribuidor)
+WHERE d.id_distribuidor IN (
+  SELECT id_distribuidor FROM nacional) AND d.id_distribuidor IN (
+  SELECT id_distribuidor FROM internacional)
+ORDER BY v.razon_social;
+  -- no retorna resultados
+
+-- 9. Liste id, nombre y apellido de los empleados que no son jefes de departamento. 
+SELECT e.id_empleado, e.nombre, e.apellido
+FROM empleado e JOIN departamento d ON e.id_empleado != d.id_departamento;
+
+-- 10. Indique si hay distribuidores (nacionales o internacionales) que efectuaron entregas a todos los videos.
+
+-- 11. Determine los videos que han recibido entregas de parte de todos los distribuidores internacionale
+SELECT v.id_video
+FROM video v JOIN entrega e USING(id_video) JOIN distribuidor d USING(id_distribuidor)
+WHERE 

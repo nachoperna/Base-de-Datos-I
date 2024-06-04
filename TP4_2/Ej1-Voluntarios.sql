37e368492e186a536c3a5353495ccbcab6f2aa01
nachoperna <105826237+nachoperna@users.noreply.github.com>
2024-06-04 15:04:17 -0300

    Update Ej1-Voluntarios.sql

--- TP4_2/Ej1-Voluntarios.sql
+++ TP4_2/Ej1-Voluntarios.sql
@@ -6,18 +6,11 @@ GROUP BY v.nro_voluntario
 ORDER BY COUNT(h.id_tarea) DESC;
 
 -- 2. Liste los datos de contacto (nombre, apellido, e-mail y teléfono) de los voluntarios que hayan desarrollado tareas con diferencia max_horas-min_horas de hasta 5000 horas y que las hayan finalizado antes del  01/01/2000.
-SELECT v.nombre, v.apellido, v.e_mail, v.telefono, v.id_tarea, (t.max_horas - t.min_horas) AS "Diferencia horas"
-FROM voluntario v JOIN tarea t ON v.id_tarea = t.id_tarea 
-WHERE v.nro_voluntario IN 
-  (SELECT h.nro_voluntario
-   FROM historico h
-   WHERE h.fecha_fin < to_date('2000-01-01','YYYY-MM-DD')
-  )
+SELECT v.nombre, v.apellido, v.e_mail, v.telefono, v.id_tarea, (t.max_horas - t.min_horas) AS "Diferencia horas", h.fecha_fin
+FROM voluntario v JOIN tarea t USING(id_tarea) JOIN historico h USING(nro_voluntario)
+WHERE h.fecha_fin < to_date('2000-01-01','YYYY-MM-DD')
   AND 
-  v.id_tarea IN (SELECT t.id_tarea
-   FROM tarea t
-   WHERE (t.max_horas - t.min_horas) > 5000
-  );
+  (t.max_horas - t.min_horas) <= 5000;
 
 -- 3. Indique nombre, id y dirección completa de las instituciones que no poseen voluntarios con aporte de horas mayor o igual que el máximo de horas de la tarea que realiza.
 SELECT nombre_institucion, id_institucion, id_direccion
@@ -27,6 +20,9 @@ WHERE id_institucion IN
    FROM voluntario v
    WHERE v.id_tarea IN (SELECT t.id_tarea FROM tarea t WHERE v.horas_aportadas < t.max_horas)
   );
+
+SELECT DISTINCT nombre_institucion, id_institucion, id_direccion
+FROM institucion JOIN voluntario v USING(id_institucion) JOIN tarea t ON t.id_tarea = v.id_tarea AND v.horas_aportadas < t.max_horas;
 
 -- 4. Liste en orden alfabético los nombres de los países que nunca han tenido acción de voluntarios (considerando sólo información histórica, no tener en cuenta los voluntarios actuales).
 SELECT nombre_pais, id_pais
@@ -58,7 +54,7 @@ WHERE id_tarea IN
 (SELECT id_tarea FROM voluntario WHERE id_institucion IN
 (SELECT id_institucion FROM institucion WHERE id_direccion IN 
 (SELECT id_direccion FROM direccion WHERE ciudad = 'Munich')))
-ORDER BY id_tarea;
+ORDER BY id_tarea HAVING COUNT(*)=1;
 
 -- 7. Indique los datos de las instituciones que poseen director, donde históricamente se hayan desarrollado tareas que actualmente las estén ejecutando voluntarios de otras instituciones.
 SELECT *
@@ -66,6 +62,9 @@ FROM institucion i
 WHERE id_director IS NOT NULL AND id_institucion IN 
 (SELECT id_institucion FROM historico WHERE id_tarea IN
 (SELECT id_tarea FROM voluntario WHERE id_institucion != i.id_institucion));
+
+SELECT DISTINCT i.*
+FROM institucion i JOIN historico h USING(id_institucion) JOIN voluntario v ON h.id_tarea = v.id_tarea AND v.id_institucion != i.id_institucion;
 
 -- 8. Listar los datos completos de todas las instituciones junto con el nombre y apellido de su director.
 SELECT *, v.apellido||', '||v.nombre AS "Director"

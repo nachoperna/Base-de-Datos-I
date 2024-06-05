c760091dcc36d32fc039d38038ff23811c67a196
nachoperna <105826237+nachoperna@users.noreply.github.com>
2024-06-05 14:58:56 -0300

    Update Ej-Peliculas.sql

--- TP4_2/Ejercicios Adicionales/Ej-Peliculas.sql
+++ TP4_2/Ejercicios Adicionales/Ej-Peliculas.sql
@@ -28,9 +28,11 @@ FROM video v
 ORDER BY v_razon_social;
 
 -- 9. Liste id, nombre y apellido de los empleados que no son jefes de departamento. 
-SELECT e.id_empleado, e.nombre, e.apellido
-FROM empleado e JOIN departamento d ON e.id_empleado != d.id_departamento;
-
+SELECT DISTINCT e.id_empleado, e.nombre, e.apellido
+FROM empleado e
+WHERE e.id_empleado NOT IN(
+SELECT jefe_departamento FROM departamento)
+	
 -- 10. Indique si hay distribuidores (nacionales o internacionales) que efectuaron entregas a todos los videos.
 
 -- 11. Determine los videos que han recibido entregas de parte de todos los distribuidores internacionale

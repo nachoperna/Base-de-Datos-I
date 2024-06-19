c2cd2012b1f905df0cde809ed12c6e60ceb89196
nachoperna <105826237+nachoperna@users.noreply.github.com>
2024-06-18 23:33:12 -0300

    Update Ej3.sql

--- TP4_2/Ej3.sql
+++ TP4_2/Ej3.sql
@@ -1,11 +1,21 @@
 -- Tomando el esquema de Voluntarios, resuelva las siguientes consultas en SQL utilizando operadores (o simulando la operación) de Conjuntos. Discuta qué sucede si se incluye el operador ALL.
 
 -- 1. Indique cuáles instituciones tienen solo un voluntario trabajando y ninguna tarea desarrollada -históricamente- hasta el momento.
-      SELECT id_institucion, COUNT(*) AS "Cantidad voluntarios trabajando"
-      FROM institucion 
-      GROUP BY id_institucion HAVING COUNT(id_institucion IN
-      (SELECT id_institucion FROM voluntario WHERE id_tarea IS NULL AND nro_voluntario NOT IN
-      (SELECT nro_voluntario FROM historico))) = 1;
+      SELECT id_institucion
+      FROM institucion
+      JOIN voluntario v USING(id_institucion)
+      GROUP BY id_institucion
+      HAVING COUNT(*) = 1
+      AND NOT EXISTS (
+          SELECT 1
+          FROM historico h
+          WHERE h.id_institucion = institucion.id_institucion
+      );
+
+      SELECT id_institucion, COUNT(*)
+      FROM institucion JOIN voluntario v USING(id_institucion) 
+      WHERE id_institucion NOT IN (SELECT id_institucion FROM historico) OR id_institucion IN (SELECT id_institucion FROM historico WHERE id_tarea IS NULL)
+      GROUP BY id_institucion HAVING COUNT(*)=1;
 
 -- 2. Liste el id, nombre y apellido de los voluntarios de instituciones asentadas en el continente ‘Europeo’ o que son coordinadores, y que además cumplen con el rol de director de alguna institución. Ordene el resultado alfabéticamente. 
       SELECT nro_voluntario, nombre, apellido
@@ -18,13 +28,18 @@
       (SELECT id_continente FROM continente WHERE nombre_continente = 'Europeo')))))
       ORDER BY apellido;
 
-      SELECT *
+      SELECT v.nro_voluntario, v.nombre, v.apellido
       FROM voluntario v
-               JOIN institucion i USING (id_institucion)
-               JOIN direccion d USING (id_direccion)
-               JOIN pais p USING(id_pais)
-               JOIN continente c USING(id_continente)
-      WHERE c.nombre_continente != 'Europeo';
+      WHERE EXISTS
+       (SELECT 1 FROM institucion i WHERE i.id_director = v.nro_voluntario)
+       INTERSECT
+      SELECT v.nro_voluntario, v.nombre, v.apellido
+      FROM voluntario v
+      WHERE v.id_coordinador IS NULL OR EXISTS
+       (SELECT 1 FROM institucion i WHERE v.id_institucion=i.id_institucion AND EXISTS
+       (SELECT 1 FROM direccion d WHERE d.id_direccion=i.id_direccion AND EXISTS
+       (SELECT 1 FROM pais p WHERE p.id_pais=d.id_pais AND EXISTS
+       (SELECT 1 FROM continente c WHERE c.id_continente=p.id_continente AND lower(nombre_continente)='europeo'))));
 
 -- 3. Liste todos los voluntarios que no pertenecen a instituciones ubicadas en el continente Europeo.
       SELECT nro_voluntario, nombre, apellido
